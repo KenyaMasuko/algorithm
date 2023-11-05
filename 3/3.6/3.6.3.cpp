660b647d74c358fdc338ac652480f48f3ce21702
kenya12814 <85816730+KenyaMasuko@users.noreply.github.com>
2023-11-05 20:31:24 +0900

    recursive algorithm

---
+++ 3/3.6/3.6.3.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int func(int N) {
+	if(N == 1) return 1;
+	return func(N - 1) + N;
+}
+
+int main() {
+	int N;
+	cin >> N;
+	cout << func(N) << endl;
+	return 0;
+}
